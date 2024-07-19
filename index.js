2c604e7ce7540e11858582aa2be50cdfcee1e2a0
Jae Jeong <jeongjj29@gmail.com>
2024-07-19 14:13:00 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch ("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            "name": name,
+            "email": email
+        })
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            const obj = document.body.appendChild(document.createElement("div"));
+            obj.innerHTML = `<p id="${object.id}"></p>`
+        })
+        .catch(function (error) {
+            alert("There has been an error.");
+            document.body.innerHTML = `<p>${error}</p>`
+        });
+}
+
+function newObj(obj) {
+    
+}
